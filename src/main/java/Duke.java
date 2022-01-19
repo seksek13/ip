5b6d4f9faa57eb224978a50bc3e426aca838f515
seksek13 <e0556272@u.nus.edu>
2022-01-19 18:40:57 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -7,14 +7,23 @@ public class Duke {
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
         boolean exit = false;
+        ArrayList<String> list = new ArrayList<>(100);
 
         while(!exit) {
             String input = sc.nextLine();
             if (input.equals("bye")) {
                 exit = true;
                 System.out.println("Bye. Hope to see you again soon!");
-            } else {
-                System.out.println(input);
+            }
+            else if(input.equals("list")){
+                int listSize = list.size()+1;
+                for(int i=1;i<listSize;i++){
+                    System.out.println(i +". " + list.get(i-1));
+                }
+            }
+            else {
+                list.add(input);
+                System.out.println("added: " + input);
             }
         }
     }
