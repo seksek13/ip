6210621bd19136a7d085c4715454df0934007bb2
seksek13 <e0556272@u.nus.edu>
2022-02-09 19:12:23 +0800

    A-CodeQualitty

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -3,6 +3,7 @@ package duke;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -16,7 +17,7 @@ public class Duke {
     /**
      * Constructor for Duke class
      *
-     * @param filePath
+     * @param filePath path to where the file is stored
      */
     public Duke(String filePath) {
         ui = new Ui();
@@ -31,88 +32,120 @@ public class Duke {
     /**
      * Method to run entire programme
      */
+    @SuppressWarnings("checkstyle:Indentation")
     public String getResponse(String input) throws DukeException {
 
         try {
-            String[] commands = Parser.parse(input);
+            String[] commands = Parser.parse(input.toLowerCase(Locale.ROOT));
             if (commands[0].equals("bye")) {
-                new Timer().schedule(new TimerTask() {
-                    public void run () { System.exit(0); }
-                }, 2000);
-                return ui.showBye();
-
+                return exitSystem();
             } else if (commands[0].equals("list")) {
-                return ui.showTaskList(tasks);
+                return listTask();
             } else if (commands[0].equals("mark")) {
                 int indexOfTask = Integer.parseInt(commands[1]);
-                // index is not in array
-                if (indexOfTask > tasks.size()) {
-                    return ui.showOutOfArray();
-                } else {
-                    Task mark = tasks.markTask(indexOfTask - 1);
-                    storage.updateTaskStatus(indexOfTask, true);
-                    return ui.showMarkTask(mark);
-                }
+                return markTask(indexOfTask);
             } else if (commands[0].equals("unmark")) {
                 int indexOfTask = Integer.parseInt(commands[1]);
-                // index is not in array
-                if (indexOfTask > tasks.size()) {
-                    return ui.showOutOfArray();
-                } else {
-                    Task mark = tasks.unmarkTask(indexOfTask - 1);
-                    storage.updateTaskStatus(indexOfTask, false);
-                    return ui.showUnmarkTask(mark);
-                }
+                return unmarkTask(indexOfTask);
             } else if (commands[0].equals("todo")) {
-                Task t = new Todo(commands[1], false);
-                tasks.addTask(t);
-                String data = "T,0," + commands[1];
-                storage.addToFile(data);
-                return ui.showAddTask(t, tasks.size());
+                return addTodoTask(commands[1]);
+            } else if (commands[0].equals("event")) {
+                return addEventTask(commands[1], commands[2]);
             } else if (commands[0].equals("deadline")) {
-                //format date time
-                DateTimeFormatter format = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
-                LocalDateTime formattedDate = LocalDateTime.parse(commands[2], format);
-                Task t = new Deadline(commands[1], formattedDate, false);
-                tasks.addTask(t);
-                String data = "D,0," + commands[1] + "," + commands[2];
-                storage.addToFile(data);
-                return ui.showAddTask(t, tasks.size());
-            } else if (input.startsWith("event")) {
-                DateTimeFormatter format = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
-                LocalDateTime formattedDate = LocalDateTime.parse(commands[2], format);
-                Task t = new Event(commands[1], formattedDate, false);
-                tasks.addTask(t);
-                String data = "E,0," + commands[1] + "," + commands[2];
-                storage.addToFile(data);
-                return ui.showAddTask(t, tasks.size());
-            } else if (input.startsWith("delete")) {
+                return addDeadlineTask(commands[1], commands[2]);
+            } else if (commands[0].equals("delete")) {
                 int index = Integer.parseInt(commands[1]);
-                // index is not in array
-                if (index > tasks.size()) {
-                    return ui.showOutOfArray();
-                } else {
-                    Task t = tasks.deleteTask(index - 1);
-                    storage.deleteFileData(index);
-                    return ui.showDeleteTask(t, tasks.size());
-                }
-            } else if (input.startsWith("find")) {
-                TaskList t = new TaskList();
-                Task[] temp = tasks.find(commands[1]);
-                int count = 0;
-                while (temp[count] != null) {
-                    t.addTask(temp[count]);
-                    count++;
-                }
-                return ui.showFindTask(t);
+                return deleteTask(index);
+            } else if (commands[0].equals("find")) {
+                return findTasks(commands[1]);
             } else {
                 return "You have entered an invalid command! :(";
             }
         } catch (DukeException | IOException | IllegalArgumentException e) {
             return ui.showError(e.getMessage());
         }
-
     }
 
+    private String exitSystem() {
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                System.exit(0);
+            }
+        }, 2000);
+        return ui.showBye();
+    }
 
+    private String listTask() {
+        return ui.showTaskList(tasks);
+    }
+
+    private String markTask(int indexOfTask) throws IOException {
+        // index is not in array
+        if (indexOfTask > tasks.size()) {
+            return ui.showOutOfArray();
+        }
+        Task taskToMark = tasks.markTask(indexOfTask - 1);
+        storage.updateTaskStatus(indexOfTask, true);
+        return ui.showMarkTask(taskToMark);
+    }
+
+    private String unmarkTask(int indexOfTask) throws IOException {
+        // index is not in array
+        if (indexOfTask > tasks.size()) {
+            return ui.showOutOfArray();
+        }
+        Task taskToUnmark = tasks.unmarkTask(indexOfTask - 1);
+        storage.updateTaskStatus(indexOfTask, false);
+        return ui.showUnmarkTask(taskToUnmark);
+    }
+
+    private String addTodoTask(String description) throws IOException {
+        Task todoTask = new Todo(description, false);
+        tasks.addTask(todoTask);
+        String data = "T,0," + description;
+        storage.addToFile(data);
+        return ui.showAddTask(todoTask, tasks.size());
+    }
+
+    private String addDeadlineTask(String description, String date) throws IOException {
+        //format date time
+        DateTimeFormatter format = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+        LocalDateTime formattedDate = LocalDateTime.parse(date, format);
+        Task deadlineTask = new Deadline(description, formattedDate, false);
+        tasks.addTask(deadlineTask);
+        String data = "D,0," + description + "," + date;
+        storage.addToFile(data);
+        return ui.showAddTask(deadlineTask, tasks.size());
+    }
+
+    private String addEventTask(String description, String date) throws IOException {
+        DateTimeFormatter format = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+        LocalDateTime formattedDate = LocalDateTime.parse(date, format);
+        Task eventTask = new Event(description, formattedDate, false);
+        tasks.addTask(eventTask);
+        String data = "E,0," + description + "," + date;
+        storage.addToFile(data);
+        return ui.showAddTask(eventTask, tasks.size());
+    }
+
+    private String deleteTask(int indexOfTask) throws IOException {
+        if (indexOfTask > tasks.size()) {
+            return ui.showOutOfArray();
+        }
+        Task taskToDelete = tasks.deleteTask(indexOfTask - 1);
+        storage.deleteFileData(indexOfTask);
+        return ui.showDeleteTask(taskToDelete, tasks.size());
+    }
+
+    private String findTasks( String description) {
+        TaskList taskList = new TaskList();
+        Task[] tasksFound = tasks.find(description);
+        int count = 0;
+        while (tasksFound[count] != null) {
+            taskList.addTask(tasksFound[count]);
+            count++;
+        }
+        return ui.showFindTask(taskList);
+    }
 }
